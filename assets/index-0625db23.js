13b07ce258b36188f32088b40e09923f7bbdf50a
wxtSky <79189721+wxtsky@users.noreply.github.com>
2023-02-28 22:11:52 +0800

    Delete assets directory

--- assets/index-0625db23.js
+++
@@ -1,1 +0,0 @@
-import{h as e,j as o}from"./index-0d81f36f.js";import{I as r}from"./index-82c67ad9.js";import"./button-10d5930a.js";import"./index-6ec23b91.js";import"./useMergedState-4984f5cf.js";function s(){return e("div",{children:[o("h1",{children:"PinkSale"}),o(r,{addonBefore:"RPC"}),o(r,{addonBefore:"预售合约"}),o(r,{addonBefore:"私钥"}),o("span",{})]})}export{s as default};
