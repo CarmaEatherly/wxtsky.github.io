8b3a7aae61caf38dd66ba3702966ac52df317baa
wxtSky <79189721+wxtsky@users.noreply.github.com>
2023-03-01 22:08:21 +0800

    Delete assets directory

--- assets/index-e525f5c1.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-02b61a1a.js";function i(){return e("div",{children:e("div",{className:"title",children:e("div",{className:"title-text",children:"批量转账"})})})}export{i as default};
