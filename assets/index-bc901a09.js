74290c5e3d2238fd2895cce5ff4eb17a7e9f8aee
wxtSky <79189721+wxtsky@users.noreply.github.com>
2023-03-01 22:23:14 +0800

    Delete assets directory

--- assets/index-bc901a09.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7089f0c5.js";function i(){return e("div",{children:e("div",{className:"title",children:e("div",{className:"title-text",children:"批量转账"})})})}export{i as default};
