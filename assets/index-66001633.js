c23d6b65aa4d756853eb02a3ab4be5c64cc55b30
wxtSky <79189721+wxtsky@users.noreply.github.com>
2023-03-01 22:10:06 +0800

    Add files via upload

---
+++ assets/index-66001633.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-30ca824a.js";function i(){return e("div",{children:e("div",{className:"title",children:e("div",{className:"title-text",children:"批量转账"})})})}export{i as default};
